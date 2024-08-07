773b9d67b6fc1b061bd10955d6e38b99fdb11260
RoniDataloopAi <roni.a@dataloop.ai>
2024-08-07 17:59:29 +0300

    new sdk

--- adapters/chat_completion/chat_completion.py
+++ adapters/chat_completion/chat_completion.py
@@ -1,7 +1,6 @@
-import openai
 import dtlpy as dl
 import logging
-import json
+import openai
 import os
 
 logger = logging.getLogger('openai-adapter')
@@ -23,23 +22,60 @@ class ModelAdapter(dl.BaseModelAdapter):
         response = self.client.chat.completions.create(
             messages=messages,
             stream=True,
-            model='gpt-4o'
+            model=self.configuration.get('model_name', 'gpt-4o')
         )
+
+        ans = ""
         for chunk in response:
-            yield chunk.choices[0].delta.content or ""
+            yield ans + (chunk.choices[0].delta.content or "")
 
     def prepare_item_func(self, item: dl.Item):
-        return item
+        prompt_item = dl.PromptItem.from_item(item)
+        return prompt_item
 
-    def predict(self, batch: [dl.Item], **kwargs):
-        """
-        API call for Openai on the items batch
-        :param batch: list of dl.Items
-        :param kwargs:
-        :return:
-        """
-        annotations = []
-        for item in batch:
-            for chunk in self.stream_response(messages=item.messages):
-                item.add_response(chunk)
-            return annotations
+    def predict(self, batch, **kwargs):
+        system_prompt = self.model_entity.configuration.get('system_prompt', "")
+        for prompt_item in batch:
+            # prompt_item._get_assistant_prompts(model_name=self.model_entity.name)
+            messages = prompt_item.to_messages(
+                model_name=self.model_entity.name)  # Get all messages including model annotations
+            messages.insert(0, {"role": "system",
+                                "content": system_prompt})
+            nearest_items = prompt_item.prompts[-1].metadata.get('nearestItems', [])
+            if len(nearest_items) > 0:
+                # build context
+                context = ""
+                for item_id in nearest_items:
+                    context_item = dl.items.get(item_id=item_id)
+                    source = context_item.metadata['system'].get('document', dict()).get('source', "missing")
+                    with open(context_item.download(), 'r', encoding='utf-8') as f:
+                        text = f.read()
+                    context += f"\n<source>\n{source}\n</source>\n<text>\n{text}\n</text>"
+                messages.append({"role": "assistant", "content": context})
+
+            stream = self.stream_response(messages=messages)
+            response = ""
+            for chunk in stream:
+                #  Build text that includes previous stream
+                response += chunk
+                prompt_item.add(message={"role": "assistant",
+                                         "content": [{"mimetype": dl.PromptType.TEXT,
+                                                      "value": response}]},
+                                stream=True,
+                                model_info={'name': self.model_entity.name,
+                                            'confidence': 1.0,
+                                            'model_id': self.model_entity.id})
+
+        return []
+
+
+if __name__ == '__main__':
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
+    dl.setenv('prod')
+    model = dl.models.get(model_id="66a8f9e2c7aab26441aa5869")
+    item = dl.items.get(item_id="66b369a82e90de89dde976e0")
+    a = ModelAdapter(model)
+    a.predict_items([item])
