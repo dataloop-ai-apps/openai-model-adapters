cc293155ebfeea56df1205ad6a7e973aedfe37a0
ShadiMah <shadi.m@dataloop.ai>
2024-08-20 11:09:07 +0300

    adding upload features flag

--- adapters/text_embeddings/text_embeddings.py
+++ adapters/text_embeddings/text_embeddings.py
@@ -20,8 +20,10 @@ class TextEmbeddings(dl.BaseModelAdapter):
         embeddings = []
         for item in batch:
             if isinstance(item, str):
+                self.adapter_defaults.upload_features = True
                 text = item
             else:
+                self.adapter_defaults.upload_features = False
                 try:
                     prompt_item = dl.PromptItem.from_item(item)
                     is_hyde = item.metadata.get('prompt', dict()).get('is_hyde', False)
