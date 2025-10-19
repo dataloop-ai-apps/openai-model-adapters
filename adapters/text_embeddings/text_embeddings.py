55a8ee3a1651ecf7b7c2a428ef4d4a16a7cf4d73
ShadiMah <shadi.m@dataloop.ai>
2025-10-19 17:03:22 +0300

    fix

--- adapters/text_embeddings/text_embeddings.py
+++ adapters/text_embeddings/text_embeddings.py
@@ -27,32 +27,42 @@ class TextEmbeddings(dl.BaseModelAdapter):
         )
         embedding = response.data[0].embedding
         return embedding
+    
+    def get_text(self, item):
+        hyde_model_name = self.configuration.get('hyde_model_name')
+        text = None
+        if isinstance(item, str):
+            text = item
+        else:
+            try:
+                prompt_item = dl.PromptItem.from_item(item)
+                is_hyde = item.metadata.get('prompt', dict()).get('is_hyde', False)
+                if is_hyde is True:
+                    messages = prompt_item.to_messages(model_name=hyde_model_name)[-1]
+                    if messages['role'] == 'assistant':
+                        text = messages['content'][-1]['text']
+                    else:
+                        raise ValueError(f'Only assistant messages are supported for hyde model')
+                else:
+                    messages = prompt_item.to_messages(include_assistant=False)[-1]
+                    text = messages['content'][-1]['text']
+
+            except ValueError as e:
+                raise ValueError(f'Only mimetype text or prompt items are supported {e}')
+        return text
 
     def embed(self, batch, **kwargs):
-        hyde_model_name = self.configuration.get('hyde_model_name')
         embeddings = []
 
         for item in batch:
-            if isinstance(item, str):
-                self.adapter_defaults.upload_features = True
-                text = item
+            text = self.get_text(item)
+            if self.configuration.get('external_vector_db', False) is False:
+                if isinstance(item, str):
+                    self.adapter_defaults.upload_features = True
+                else:
+                    self.adapter_defaults.upload_features = False
             else:
                 self.adapter_defaults.upload_features = False
-                try:
-                    prompt_item = dl.PromptItem.from_item(item)
-                    is_hyde = item.metadata.get('prompt', dict()).get('is_hyde', False)
-                    if is_hyde is True:
-                        messages = prompt_item.to_messages(model_name=hyde_model_name)[-1]
-                        if messages['role'] == 'assistant':
-                            text = messages['content'][-1]['text']
-                        else:
-                            raise ValueError(f'Only assistant messages are supported for hyde model')
-                    else:
-                        messages = prompt_item.to_messages(include_assistant=False)[-1]
-                        text = messages['content'][-1]['text']
-
-                except ValueError as e:
-                    raise ValueError(f'Only mimetype text or prompt items are supported {e}')
 
             embedding = self.call_model(text=text)
             logger.info(f'Extracted embeddings for text {item}: {embedding}')
